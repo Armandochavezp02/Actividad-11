81d336f4c9ef57e64fecda3ee33a9e8d034904b8
Armando Chavez <a3160996@uach.mx>
2021-04-29 22:24:21 -0600

    primer commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const todo = (tarea, duracion, callback) => {
+    setTimeout(()=>{
+        callback(tarea)
+    }, duracion)
+}
+
+todo("Levantarse", 5000, (tarea) => {
+    console.log(`realizando la tarea ${tarea}`)
+        
+    todo("Tender la cama", 3000, (tarea) => {
+        console.log(`realizando la tarea ${tarea}`)
+    
+        todo("Bañarse", 3000, (tarea) => {
+            console.log(`realizando la tarea ${tarea}`)
+        
+            todo("Clase de paradigmas", 5000, (tarea) => {
+                console.log(`realizando la tarea ${tarea}`)
+            
+                todo("Desayunar", 8000, (tarea) => {
+                    console.log(`realizando la tarea ${tarea}`)
+                
+                    todo("Jugar", 10000, (tarea) => {
+                        console.log(`realizando la tarea ${tarea}`)
+                    })
+                })
+            })
+        })
+    })
+})
